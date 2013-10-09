6c94d3f104a98a4e288105a69c87f8aed7b77b4f
Adrian Velazquez <adriansane@fullsail.edu>
2013-10-09 19:55:07 -0400

    Expressions assignment completed
    moving along to the next assignments

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -94,3 +94,65 @@ var fullName = firstName + " " + lastName; /*adding a second + and a blank space
 first name and my last name*/
 console.log(fullName);
 
+//Casting - Treating strings like numbers and numbers like strings
+//Casting types: arrays, numbers, booleans, strings, etc.
+//CASTING STRINGS AS NUMBERS
+var stringVar = "6"; //This holds he string 6 not the numerical character 6. (its in parenthesis)
+var result = 7 + stringVar;
+console.log(result);
+
+/*in order to get a computer to treat this result variable as a number rather than a string, you enclose the variable in parenthesis and add the word Number (capital N)*/
+
+var stringVar2 = "6";
+var result2 = 7 + Number(stringVar2); // treats stringVar like a number rather than a string because of (Number and ())
+console.log(result2);
+//this tells the machine to treat it or cast it as a number rather than a string
+
+//CASTING NUMBERS AS STRINGS
+ var areaCode = 718;
+ var firstPart = 585;
+ var secondPart = 8987;
+ var phoneNo = String(areaCode) + String(firstPart) + String(secondPart); //remember to use capital letters when casting
+ console.log(phoneNo);
+
+//to take it a step farther and add formatting to the number, we can concatenate using "quotation marks"
+//(718) 585-8987
+var area = 718;
+var first = 585;
+var second = 8987;
+var fullPhone = "(" + String(area) + ")" + String(first) + "-" + String(second);
+console.log(fullPhone);
+
+//Expressions - assignment operators: * + - / += *= /+ -=
+
+var a = 3;
+a = a + 4; //this arithmetic expression adds the value of a to 4 a = 3 + 4 = 7 and also assigns the expression tag with a value of 3 by using a
+console.log(a);
+
+//another way to write this expresion would be to use the += assignment operator
+ var a = 4;
+ a += 6; // this tells the machine to add the value of a to 6. the answer of 4 is stored within the a.
+ //a = a + 4; is the same as writing a += 4;
+ console.log(a);
+ 
+ //remember you have to assign expressions to variables - whats a? a = 4
+ 
+ // += this operator adds and changes the value
+ // -= this operator subtracts and changes the value
+ // *= this operator multiplies and changes the value
+ // /= this operator divides and changes the value
+ 
+ //Expressions - increment and decrement operators only work with values of 1
+ //To increment we use ++
+ //To decrement we use --
+ 
+ var a = 1000;
+ a ++; //++ adds 1 to 1000
+ console.log(a);
+ 
+ var a = 2000;
+ a --; //-- subtracts 2000 to 1999
+ console.log(a);
+ 
+ 
+ 
