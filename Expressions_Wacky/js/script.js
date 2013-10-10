3407bce4d19cbe9d0b9714c01fb2358a7f2b01e5
Adrian Velazquez <adriansane@fullsail.edu>
2013-10-10 19:30:00 -0400

    adding criteria
    adding criteria to each assignment for better production. These
    comments will be used to ensure that I follow the criteria and rubric
    and get a good grade. Comment will be deleted from all assignments
    before final submission.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,3 +1,30 @@
 // Adrian Velazquez
 // October 10th 2013
 // Expressions_Wacky
+
+/* Make sure each of the projects follows this list of criteria:
+You will need to create code that calculates a solution. The actual values don't matter here. Anyone should be able to open your JavaScript file, change the values in your variables and the calculation should still be accurate!
+Decide on what your code will calculate. This should be an original calculation. In other words you may not use any of the examples mentioned in this class. This will result in an automatic 0.
+Your calculation should use a minimum of three factors or givens.
+Good example: Using width, height and depth to calculate volume.
+Bad example: Using time and distance to calculate speed.
+All three factors must be essential for the calculation. If one of the factors (the pieces of information you are collecting with the prompts) is not needed in order to calculate the result then it is not essential.
+There should be a minimum of 3 prompts to collect user information. The prompts should include labels so the user knows what information they need to enter. These prompts should assign the information collected into variables you have set up.
+Your calculation should be original.
+Write the givens as appropriately named variables.
+Declare a variable for the resulting value of the calculation.
+Calculate the result using the variables in an expression (NOT the literal values or numbers)
+Assign the calculation to the resulting variable.
+The result should appear in the browser's console. (Remember to include units like “square feet” or "meters cubed"). This should explain the result as well.
+Good example of console print out: The volume of the sphere is 26 feet cubed.
+Bad example of console print out: 26
+Show at least one example of string concatenation (this will be useful for that console print out mentioned above).
+Comment every line of code (describe what each line is doing in English). Do NOT just label sections of your code.
+Each of the following requirements must exist in at least one of the 3 subprojects:
+An example of 3 different arithmetic operators.
+An example of an array. This array must be used as part of the calculation.
+A string variable
+At least 2 assignment operators (not including the = symbol)
+Committing to your repository:
+You are required to make at least 20 commits to your repository for each project as you develop your code.
+Failing to do so will result in a 0 for the assignment. */
