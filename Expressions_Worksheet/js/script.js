d9bab837d0bd1376027c2789dbaa3464b9f25599
Adrian Velazquez <adriansane@fullsail.edu>
2013-10-09 21:23:09 -0400

    Finished answering problem 3

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -43,51 +43,6 @@ console.log(“The circumference of the circle is “ + circ );
 
 Problems:
 
-Dog Years
-
-Dogs age 7 times faster than humans so a dog that is 1 year old in human years is 7 years old in “dog years.”  Calculate how old Sparky the pit bull is in dog years based on his actual age.
-     Givens:
-Sparky’s age.
-     Result Varable:
-Sparky’s age in dog years.
-    Result to Print:
-“Sparky is X human years old which is X in dog years.
-
-Slice of Pie part 1
-
-A bunch of students are having a party and somebody ordered pizza. Create an expression that calculates how much pizza each partygoer will get at the party.  (Assume all pizzas have the same number of slices and that the person dividing the pizza is really precise, so this can be a decimal, like 3.52 slices, etc.)
-     Given:
-Number of slices per pizza
-Number of people at the party
-Number of pizzas ordered.
-     Result Variables:
-Number of slices per person. (Can be a decimal or floating point).
-    Result to Print:
-“Each person ate X slices of pizza at the party.”
-
-Slice of Pie part II
-
-At the pizza party Sparky, the host’s dog is excited, because he gets the leftover pizza after the slices have been divided up evenly among the guests. Assume guests get whole slices, how many whole slices will Sparky feast on?
-Example data set: 10 people, 4 pizzas and 8 slices per pizza will mean each person eats 3 slices and Sparky gets 2 slices. (Note that this is an example, your code should work and give me the accurate results no matter what numbers I put in for those given variables.)
-     Given:
-Don’t make new given variables/constants for this. Instead use the givens you set up for Slice of Pie I.
-     Result Variables:
-Number of slices Sparky gets to eat.
-    Result to Print:
-“Sparky got X slices of pizza.”
-
-Average shopping bill
-
-You are budgeting your money for the year and must calculate your average weekly grocery shopping spending over the past five weeks. Store the past five grocery totals as a list in an array. Create an expression that will use the items in the array to calculate the average amount spent on groceries.
-Please note: there should only be one given for this problem set, the array holding the five weekly totals.  You should be able to access the array using the array access notation discussed earlier in the course.
-     Givens:
-An array with five weekly grocery totals.
-     Result Variable:
-Total amount spent on groceries.
-Average weekly grocery spending.
-    Result to Print:
-“You have spent a total of $X on groceries over 5 weeks. That is an average of $X per week”
-
 Discounts
 
 Calculate the discounted price for an item. Create an expression that will calculate the discounted price with and without sales tax. (It is acceptable for the result to have more than two digits after the decimal. $345.896 for example.)
@@ -110,9 +65,25 @@ console.log(age); //this will print the dogs years out in human years.
 
 //Slice of Pie part 1
 var slices = 12; //this variable represents the given number of slices per pizza.
-var people = 24; //this variable represents the given number of people at the party.
+var people = 20; //this variable represents the given number of people at the party.
 var pizzas = 4; //this variable represents the given number of pizzas delivered to the party.
-
-var perPerson = slices * pizzas / people; /*this variable multiplies the slices and pizzas to give me the total number of slices, then divides the answer by the number of people to give me the amount of slices per people at the party.*/
+var perPerson = slices * pizzas / people; /*this variable multiplies the slices 12, and pizzas 4, to give me the total number of slices 48, then divides the answer by the number of people 20, to give me the amount of slices per people at the party 2.4 */
 console.log(perPerson); //this will print the amount of slices per person at the party.
+
+// Slice of Pie part II
+//Please see givens for Slice of Pie part 1
+var sparky = 48 % 20; /*the total number of slices divided by the number of even slices per person leave a remainder of 8 slices for sparky.*/
+console.log(sparky); //sparky got 8 slices of pizza
+
+//Average shopping bill
+
+/*You are budgeting your money for the year and must calculate your average weekly grocery shopping spending over the past five weeks. Store the past five grocery totals as a list in an array. Create an expression that will use the items in the array to calculate the average amount spent on groceries.
+Please note: there should only be one given for this problem set, the array holding the five weekly totals.  You should be able to access the array using the array access notation discussed earlier in the course.
+     Givens:
+An array with five weekly grocery totals.
+     Result Variable:
+Total amount spent on groceries.
+Average weekly grocery spending.
+    Result to Print:
+“You have spent a total of $X on groceries over 5 weeks. That is an average of $X per week”*/
 
