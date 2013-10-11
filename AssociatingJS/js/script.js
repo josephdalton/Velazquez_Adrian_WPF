305f775632141bce80934b12e690515bdffbb4d4
Adrian Velazquez <adriansane@fullsail.edu>
2013-10-11 19:05:08 -0400

    continuing to add to assignment

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -159,4 +159,8 @@ console.log(a);
  var userInput = prompt("Enter your year of birth:");
  console.log(userInput);
  
+ var width = prompt("We are calculating the area of a rectangle.\n Please enter the width:");
+ var height = prompt("Please enter the height:");
+ 
+ 
  
