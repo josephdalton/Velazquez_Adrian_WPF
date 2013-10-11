5f442d654eb89514a00f764acc9583a2d8a91c63
Adrian Velazquez <adriansane@fullsail.edu>
2013-10-10 22:58:03 -0400

    reformatting

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -32,8 +32,11 @@ var trip1 = 122; //the variable trip1 shows the number of miles I went on one ta
 var trip2 = 119; //the variable trip2 shows the number of miles I went on one tank of gas the following week - This is a given.
 var trip3 = 110; //the variable trip3 shows the number of miles I went on one tank of gas the following week - This is a given.
 var trip4 = 126; //the variable trip4 shows the number of miles I went on one tank of gas the last week - This is a given.
+
 var average = (trip1 + trip2 + trip3 + trip4)/4; //this adds the average mileage for four fill ups and divides by 4 to find the average mileage
+
 var miles = (average / tankCap); //this divides the average mileage by the capacity of the tank to find my miles per gallon
+
 var per100 = (100 / miles); //this divides the number of miles I need to travel by the miles per gallon I get from my car
 console.log("It would take", per100, "to travel 100 miles"); //the answer to the question
 
