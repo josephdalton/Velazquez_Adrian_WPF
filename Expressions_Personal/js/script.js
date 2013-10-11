82d9d8659f341aebaf32ce9469b785751fa8648b
Adrian Velazquez <adriansane@fullsail.edu>
2013-10-10 22:55:37 -0400

    formatting code

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -28,20 +28,13 @@ Good example of console print out: The volume of the sphere is 26 feet cubed.
 //how many gallons of gasoline do I need to travel 100 miles
 
 var tankCap = 13; //the variable tank cap is the storage container for 13 gallons of gasoline - This is a given.
-
 var trip1 = 122; //the variable trip1 shows the number of miles I went on one tank of gas
 var trip2 = 119; //the variable trip2 shows the number of miles I went on one tank of gas the following week
 var trip3 = 110; //the variable trip3 shows the number of miles I went on one tank of gas the following week
 var trip4 = 126; //the variable trip4 shows the number of miles I went on one tank of gas the last week
-
 var average = (trip1 + trip2 + trip3 + trip4)/4; //this adds the average mileage for four fill ups and divides by 4 to find the average mileage
-
 var miles = (average / tankCap); //this divides the average mileage by the capacity of the tank to find my miles per gallon
-
 var per100 = (100 / miles); //this divides the number of miles I need to travel by the miles per gallon I get from my car
-
-
-
 console.log("It would take", per100, "to travel 100 miles"); //the answer to the question
 
 
