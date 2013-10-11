bcf9d05bd42b8a2399a043faeb8cf70a8445f383
Adrian Velazquez <adriansane@fullsail.edu>
2013-10-10 23:40:08 -0400

    deleting criteria

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,26 +2,6 @@
 // October 10th 2013
 // Expressions_Wacky
 
-/* Make sure each of the projects follows this list of criteria:
-You will need to create code that calculates a solution. The actual values don't matter here. Anyone should be able to open your JavaScript file, change the values in your variables and the calculation should still be accurate!
-Decide on what your code will calculate. This should be an original calculation. In other words you may not use any of the examples mentioned in this class. This will result in an automatic 0.
-Your calculation should use a minimum of three factors or givens.
-Good example: Using width, height and depth to calculate volume.
-Bad example: Using time and distance to calculate speed.
-All three factors must be essential for the calculation. If one of the factors (the pieces of information you are collecting with the prompts) is not needed in order to calculate the result then it is not essential.
-There should be a minimum of 3 prompts to collect user information. The prompts should include labels so the user knows what information they need to enter. These prompts should assign the information collected into variables you have set up.
-Your calculation should be original.
-
-Write the givens as appropriately named variables.
-
-Declare a variable for the resulting value of the calculation.
-
-Calculate the result using the variables in an expression (NOT the literal values or numbers)
-
-Assign the calculation to the resulting variable.
-
-The result should appear in the browser's console. (Remember to include units like “square feet” or "meters cubed"). This should explain the result as well. */
-
 //Who done it - using if and else as an experiment, an array is present, trying to find the guilty part in a group of suspects to a murder
 
 var suspects = ["theCook", "theMaid", "theButler", "theNanny"]; //this array holds a list of suspects
