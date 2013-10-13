a0930fa570eb4b16cb77da66c918557bcce3ecef
Adrian Velazquez <adriansane@fullsail.edu>
2013-10-13 12:47:31 -0400

    adding arrays for further explanation of production process

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -8,11 +8,13 @@ var duties = ["Discovery", "Design Structure", "Design Visual", "Build and Integ
 
 var a = "developers"; //this depicts a developers responsability 
 var b = "designers"; //this depicts a designers responsability
-var c = "team leads"; //this depicts a teamLeads responsability
+var c = "team lead\'s"; //this depicts a teamLeads responsability
 
 console.log(duties[0] + " is a " + b + " duty "); //prints out a duty for designers
 console.log(duties[1] + " is a " + b + " duty "); //prints out a duty for designers
 console.log(duties[2] + " is a " + b + " duty "); //prints out a duty for designers
 console.log(duties[3] + " is a " + a + " duty "); //prints out a duty for developers
-console.log(duties[4] + " is a " + a + " , " + b + " , " + " and " +  c  + " duties "); //prints out a duty for all members of the team
+console.log(duties[4] + " is a " + a + ", " + b + ", " + "and " +  c  + " duties "); //prints out a duty for all members of the team
 console.log(duties[5] + " is a " + c + " duty "); //prints out a duty for team leads
+
+var subDuties = ["CLient Meetings", "Wire Frames", "Mockup\'s", "Front-End Development", "UI and Design", "Launch to Server"];
