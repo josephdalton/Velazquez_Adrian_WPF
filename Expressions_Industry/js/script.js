eb296b438b29eb4ee5b78d2b05bb0006b2a9b52c
Adrian Velazquez <adriansane@fullsail.edu>
2013-10-13 13:11:56 -0400

    Final Submission for assignment, Note to instructor in comments

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,14 +2,14 @@
 // October 10th 2013
 // Expressions_Industry
 
-//for industry, i will create a list of duties issued to designers and developers
+//for industry, i will create a list of duties issued to designers, developers, team leads, and clients
 
 var duties = ["Discovery", "Design Structure", "Design Visual", "Build and Integrate", "Test", "Launch"]; //this is my array of duties
 
 var a = "developers"; //this depicts a developers responsability 
 var b = "designers"; //this depicts a designers responsability
 var c = "team lead\'s"; //this depicts a teamLeads responsability
-var d = "client\'s";
+var d = "client\'s"; //this depicts the clients position in the production process
 
 console.log(duties[0] + " is a " + b + " duty "); //prints out a duty for designers
 console.log(duties[1] + " is a " + b + " duty "); //prints out a duty for designers
@@ -26,3 +26,5 @@ console.log(subDuties[2] + " are performed in the " + duties[2] + " phase with "
 console.log(subDuties[3] + " is performed in the " + duties[3] + " phase with " + a + ", " + c + ", " + "and " + d); //prints out steps
 console.log(subDuties[4] + " are performed in the " + duties[4] + " phase with " + a + ", " + b + ", " + "and " + c); //prints out steps
 console.log(subDuties[5] + " duties are perfomed in the " + duties[5] + " phase with the " + a + ", "+ c + ", " + "and " + "the " + d); /*prints out steps*/
+
+/*NOTE TO INSTRUCTOR: THIS IS MY LATE, FINAL, SUBMISSION FOR THIS ASSIGNMENT. SUNDAY, OCTOBER 13TH 2013. THANKS YOU FOR YOUR PATIENCE AND UNDERSTANDING*/ 
