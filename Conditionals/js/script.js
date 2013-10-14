8675cf44f3184b2cf562c8295e9b6b1bdefb91a2
Adrian Velazquez <adriansane@fullsail.edu>
2013-10-14 19:39:33 -0400

    continuing assignment

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -18,6 +18,38 @@ if(oldEnough){
 	}//code performed if the condition is true
 	
 //Relational Expressions (comparing objects for decision making)
+//conditional operators: > greater than, < less than, >= greater than or equal to, <= less than or equal to,!= not equal to, == equal to.
+
+var kidHeight = 30;
+
+//if kid height is over 48 inches
+
+if(kidHeight > 48){
+	console.log("You can ride the coaster!");
+	}//code performed if the condition is true
+
+//the above statement is false and will not print
+
+var kidHeight = 50;
+
+//if kid height is over 50 inches
+
+if(kidHeight > 20){
+	console.log("You can ride the coaster!");
+	}
+	
+//the above statement is true and will print
 
 
+//comparing a variable to another variable in the conditions
+
+var kidHeight = 50;
+var minheight = 22;
+
+//if the kidHeight is greater than the minHeight, print to the console
+if(kidHeight > minHeight){
+	console.log("You can ride the coaster!");
+	}
+	
+//the above statement is true and will print
 
