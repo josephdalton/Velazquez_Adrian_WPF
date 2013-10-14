cc01f1732911b2a8d76f7ce0496cdd118da75e1e
Adrian Velazquez <adriansane@fullsail.edu>
2013-10-14 19:10:27 -0400

    completing assignments

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -3,3 +3,9 @@
 //Web Programming Fundamentals
 //October 2013
 //Conditionals
+
+//Conditional statements are one of the four types of procedural code: sequential, CONDITIONAL, reusable, and repetitive
+
+//Conditional Logic
+
+
