8cec5c1cec35ac59dc287fb585d2b2d87365e756
Adrian Velazquez <adriansane@fullsail.edu>
2013-10-14 19:21:57 -0400

    continuing assinment

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -8,4 +8,16 @@
 
 //Conditional Logic
 
+var oldEnough = true;
+//Always write out the conditional statements in plain english first. This will help better understand the code you are writing or reading
+
+//If the child is old enough, print to the console, "you can ride!"
+
+if(oldEnough){
+	console.log("You can ride the coaster!");
+	}//code performed if the condition is true
+	
+//Relational Expressions (comparing objects for decision making)
+
+
 
