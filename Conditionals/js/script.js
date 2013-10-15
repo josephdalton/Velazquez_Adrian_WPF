75261091d168ee9fa8c5af2f44ef2fe089a7c727
Adrian Velazquez <adriansane@fullsail.edu>
2013-10-14 20:21:14 -0400

    CONTINUING ASSIGNMENT

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -6,7 +6,11 @@
 
 //Conditional statements are one of the four types of procedural code: sequential, CONDITIONAL, reusable, and repetitive
 
-//Conditional Logic
+//CONDITIONAL LOGIC
+
+/*if(condition){
+	action;
+	}//the barebones of the conditional statement*/
 
 var oldEnough = true;
 //Always write out the conditional statements in plain english first. This will help better understand the code you are writing or reading
@@ -17,7 +21,7 @@ if(oldEnough){
 	console.log("You can ride the coaster!");
 	}//code performed if the condition is true
 	
-//Relational Expressions (comparing objects for decision making)
+//RELATIONAL EXPRESSIONS (comparing objects for decision making)
 //conditional operators: > greater than, < less than, >= greater than or equal to, <= less than or equal to,!= not equal to, == equal to.
 
 var kidHeight = 30;
@@ -44,7 +48,7 @@ if(kidHeight > 20){
 //comparing a variable to another variable in the conditions
 
 var kidHeight = 50;
-var minheight = 22;
+var minHeight = 22;
 
 //if the kidHeight is greater than the minHeight, print to the console
 if(kidHeight > minHeight){
@@ -52,4 +56,31 @@ if(kidHeight > minHeight){
 	}
 	
 //the above statement is true and will print
+
+//CONDITIONS WITH EXPRESSIONS
+
+var kidHeight = 50;//+2 from condition below = 52
+var minheight = 22;
+
+//if the kidHeight is greater than the minHeight, print to the console
+if(kidHeight + 2 /* <-- this is and expression*/ > minHeight){
+	console.log("You can ride the coaster!");
+	} //this condition is true an will print
+	
+// you can compare it with a variable that represents the value of the expression (+2) as well
+
+var kidHeight = 50;//+2 from condition below = 52
+var minheight = 22;
+var sneakerLift = 2;
+
+//if the kidHeight is greater than the minHeight, print to the console
+if(kidHeight + sneakerLift /* <-- this is and expression*/ > minHeight){
+	console.log("You can ride the coaster!");
+	} //this condition is true an will print
+	
+
+//IF ELSE - CHOOSING BETWEEN TWO BLOCKS OF CODE
+
+
+
 
