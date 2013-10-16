2f73428fd54063b00a303b27c59fd5ed5a5c39e0
Adrian Velazquez <adriansane@fullsail.edu>
2013-10-15 20:34:25 -0400

    Added criteria and problems to solve

--- Conditionals_Worksheet/js/script.js
+++ Conditionals_Worksheet/js/script.js
@@ -1,9 +1,58 @@
 //Adrian Velazquez
 //October 15th 2013
-// Conditionals Worksheet
+//Conditionals Worksheet
+
+/*Criteria:
+For each solution you will need the following:
+Label the section of code appropriately
+Write the givens as variables
+The result should be calculated using variables, not literal values when possible.
+Create code that will make decisions based on the value of the givens
+Print the result of the decision-making to the console.
+Use only code and techniques learned in this class.
+*/
 
 
 
+//EXPRESSIONS WITH CONDITIONALS
+
+/*Last Chance for Gas!
+
+A driver has to determine if they can make it across the desert with their current fuel.  They are about to past the last gas station for the next 200 miles and they need to determine whether they should stop now for gas or not.
+    
+Given:
+Gas efficiency of the car (in mpg)
+Gauge reading of the gas tank (in %)
+Car’s gas tank capacity (in gallons)
+Result To Print Out:
+“Yes, you can make it without stopping for gas!” or “You only have X gallons of gas in your tank, better get gas now while you can!”*/
 
 
 
+//MULTIPLE RESULTS
+
+/*Check the Login
+
+Make sure the user has the correct username and password. If the username doesn’t match then a specific message for that should be printed to the console. If the password doesn’t match a message should appear for that, etc. Only one error message should appear.
+Given:
+Username entered by user
+Password entered by user
+Correct username
+Correct password
+Result To Print Out:
+“Welcome, (place their username here)!”  - if the username and password is correct
+“User not found. Try again.” -if the username does not match
+“Password does not match our records.”  -if the username matches but the password does not*/
+
+
+
+//MULTIPLE CONDITIONS
+
+/*Tire Pressure I
+
+To meet to maintenance standards a car’s front two tires should have the same pressure and the back two tires should have the same pressure.  But the front tires and the rear tires can have different pressure than each other, so it is not necessary for all four tires’ pressure to be the same. Create a single conditional that would determine if the tires of a given car are up to spec. 
+    
+Given:
+Pressure for each tire of the car (in psi) in an array.
+Result To Print Out:
+“The tires pass spec!” Or  “ Get your tires checked out!”*/
