97763641b9bbc5b56f15ebd6e4917e97aa0e47ee
Adrian Velazquez <adriansane@fullsail.edu>
2013-10-17 19:47:14 -0400

    more comments for code

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -30,8 +30,8 @@ At least one relational operator other than ==*/
 var savings = 3000;//this variable holds the value of how much i have in my savings account
 var bmw = 4500;//this variable holds the value of how much the bmw costs
 var paycheck =1500;//this variable holds the value of how much my paycheck is
-var payday = friday;//this variable holds the value of whether it is payday or not
-var friday = false;
+var payday = friday;//this variable holds the value of when payday is
+var friday = false;//this variable holds the value of whether it is friday or not
 //if today is payday and if i get paid 1500 dollars i can buy the bmw, if not i have to wait
 
  
