54c44fe2b01ae268dfc531fa9f4db119d7c1d35c
Adrian Velazquez <adriansane@fullsail.edu>
2013-10-17 20:57:36 -0400

    commenting more code

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -30,7 +30,7 @@ At least one relational operator other than ==*/
 //if the client is nice, i will work hard for him on this project, if the client is a bad client, i wont work as hard for him
 
 var niceClient = false;//this variable states that a seemingly nice client is false and therefore not true
-(niceClient = true) ? console.log("You\'re a bad client, I wont work as hard for you") : console.log("You\'re a nice client, I\'ll work hard for you");
+(niceClient = true) ? console.log("You\'re a bad client, I wont work as hard for you") : console.log("You\'re a nice client, I\'ll work hard for you");/*if the client is bad and thats a true statement, print the string in the first action. If the client is nice and thats a true statement, print the string in the second action */
 
 //the above code in Conditional Logic using if else statements
 
