ca704f179aaa533b9033b800756daa24280a3b1b
Adrian Velazquez <adriansane@fullsail.edu>
2013-10-17 20:13:35 -0400

    commenting more code

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -38,7 +38,7 @@ var friday = false;//this variable holds the value of whether it is friday or no
 if(payDay == true && payCheck >= 1500)/*if today is payday, and if my check is greater than or equal to 1500*/{
 	console.log("I can buy my BMW!");//if the above conditions are met, print "I can buy my BMW"
 }else if(friday = true || payCheck < 1500)/*If today is friday, then it is payday or if my paycheck is less than 1500*/{
-	console.log("I have to wait till next payday");
+	console.log("I have to wait till next payday");//if the above conditions are met, print "I have to wait till next payday"
 }else{
 	console.log("I cant buy my BMW");
 }
