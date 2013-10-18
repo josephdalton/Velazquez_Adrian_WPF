4c8525116be44d720b3c82e656a097dc8b36ca37
Adrian Velazquez <adriansane@fullsail.edu>
2013-10-17 20:05:43 -0400

    added actions, testing

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -38,7 +38,7 @@ var friday = false;//this variable holds the value of whether it is friday or no
 if(payDay == true && payCheck >= 1500){
 	console.log("I can buy my BMW!");
 }else if(friday = true || payCheck < 1500){
-	console.log("I have to wait till payday");
+	console.log("I have to wait till next payday");
 }else{
-	console.log();
+	console.log("I cant buy my BMW");
 }
