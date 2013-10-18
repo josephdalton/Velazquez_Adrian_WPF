a6ad1afe361c646fc2a3b04555e0c56998277027
Adrian Velazquez <adriansane@fullsail.edu>
2013-10-17 20:16:00 -0400

    added comments, deleted uneeded comments, final commit

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -2,29 +2,6 @@
 // October 17th 2013
 // Conditionals_Personal
 
-/*Criteria:
-
-Make sure each of the sub-projects follows this list of criteria:
-As in the Expressions assignment you will need to create code that calculates a result. You can not use the same formulas or code used in that previous assignment, however.
-Decide what your code will calculate. This should be an original calculation. In other words you may not use any of the examples mentioned in this class. This will result in an automatic 0.
-Your calculation should use a minimum of two factors or givens.
-At least two factors must be essential for the calculation. If one of the factors (the pieces of information you are collecting with the prompts) is not needed in order to calculate the result then it is not essential.
-There should be a minimum of two prompts to collect user information. The prompts should include labels so the user knows what information they need to enter. These prompts should assign the information collected into variables you have set up.
-Write the givens as appropriately named variables.
-Declare a variable for the resulting value of the calculation.
-Calculate the result using the variables in an expression (NOT the literal values or numbers)
-Assign the calculation to the resulting variable.
-The result should appear in the browser's console. (Remember to include units like “square feet” or "meters cubed"). This should explain the result as well.
-Validation
-In each of the sub-projects you must use conditionals to validate the information the user enters into the prompts.
-For example, if the user does not enter a value for the first prompt, make a conditional that checks if the value is empty. You can do this by evaluating if the contents of the prompt are the same as an empty string (which looks like "" i.e. thats two quotations with no character between them)
-Comment every line of code (describe what each line is doing in English). Do NOT just label sections of your code.
-Each of the following requirements must exist in at least one of the 3 subprojects:
-A ternary
-An else if
-A logical operator
-At least one relational operator other than ==*/
-
 //Buying my BMW
 
 var savings = 3000;//this variable holds the value of how much i have in my savings account
@@ -40,5 +17,5 @@ if(payDay == true && payCheck >= 1500)/*if today is payday, and if my check is g
 }else if(friday = true || payCheck < 1500)/*If today is friday, then it is payday or if my paycheck is less than 1500*/{
 	console.log("I have to wait till next payday");//if the above conditions are met, print "I have to wait till next payday"
 }else{
-	console.log("I cant buy my BMW");
+	console.log("I cant buy my BMW");//if both conditions are not met or true, print "I cant buy my BMW"
 }
