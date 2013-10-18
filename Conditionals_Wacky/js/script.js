7e9d193d0e37cffc8b2e043707fb5dea3f9d31d3
Adrian Velazquez <adriansane@fullsail.edu>
2013-10-17 21:41:44 -0400

    adding variable givens for wacky conditionals

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -6,10 +6,14 @@
 
 Make sure each of the sub-projects follows this list of criteria:
 As in the Expressions assignment you will need to create code that calculates a result. You can not use the same formulas or code used in that previous assignment, however.
+
 Decide what your code will calculate. This should be an original calculation. In other words you may not use any of the examples mentioned in this class. This will result in an automatic 0.
+
 Your calculation should use a minimum of two factors or givens.
+
 At least two factors must be essential for the calculation. If one of the factors (the pieces of information you are collecting with the prompts) is not needed in order to calculate the result then it is not essential.
 There should be a minimum of two prompts to collect user information. The prompts should include labels so the user knows what information they need to enter. These prompts should assign the information collected into variables you have set up.
+
 Write the givens as appropriately named variables.
 Declare a variable for the resulting value of the calculation.
 Calculate the result using the variables in an expression (NOT the literal values or numbers)
@@ -25,5 +29,7 @@ An else if
 A logical operator
 At least one relational operator other than ==*/
 
-
- 
+var foreign = false;
+var domestic = false;
+var euro = true;
+var member = true;
