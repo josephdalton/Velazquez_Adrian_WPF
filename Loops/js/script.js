f268e4f7b95a52063c229862e9e4e597377bc173
Adrian Velazquez <adriansane@fullsail.edu>
2013-10-23 18:26:43 -0400

    finsihing up do while statements

--- Loops/js/script.js
+++ Loops/js/script.js
@@ -1,9 +1,21 @@
 //LOOPS - 3 TYPES OF LOOPS: WHILE, DO WHILE, FOR
 //While loops look similar to "if" conditional statements
 
+
+//WHILE LOOPS
 var b = 10;//setsup the index
 
 while (b > 0){//checks the condition
 	console.log(b + "kegs on the wall");
 	b--//this decrements or increments the loop (index). Otherwise it would loop for infinity
 }
+
+//DO WHILE LOOPS
+var c = 10;
+
+do {
+	console.log(c + "kegs on the wall");
+	c--;
+}//this code will run
+
+while (c > 0);//as long as this condition is true
