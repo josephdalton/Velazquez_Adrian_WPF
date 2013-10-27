dd79f06b186a5312f11a4bfc93a895c91cbfe75d
Adrian Velazquez <adriansane@fullsail.edu>
2013-10-27 12:38:10 -0400

    Deletinguneeded code

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -9,15 +9,7 @@ Information should be collected from the user with prompts
 Calculate the result using the variables in an expression (NOT the literal values or numbers)
 The result should appear in the browser's console. (Remember to include units like “square feet” or "meters cubed"). This should explain the result as well.
 Use a function as part of your code in each sub-project. This function should have at least two parameters and return a value.
- 
-Each of the following requirements must exist in at least one of the 3 subprojects:
-A ternary
-An expression with two arithmetic operators
-Don't forget!:
-
-Test your code! Do the calculations on your own calculator. Check that the computer is producing the same result.
-Remember that functions are sub factories or sub programs of the entire program.
-You are required to make a minimum of 20 commits to your Git repository as you progress and write your code. Failing to do so will result in an automatic 0 for the assignment.*/
+*/
 
 //Friday is right around the corner. False! Chronological distance cannot be measured geometrically. lol 
 
