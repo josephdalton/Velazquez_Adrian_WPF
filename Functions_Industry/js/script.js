897a0afb981eaa66ac03f3b752ae88f6df322741
Adrian Velazquez <adriansane@fullsail.edu>
2013-10-27 12:07:42 -0400

    functions industry assignment complete

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,21 +2,6 @@
 //October 24th 2013
 //Functions - Industry
 
-/*
-Each of the following requirements must exist in at least one of the 3 subprojects:
-A ternary
-An else if
-An anonymous function
-A normal "named" function
-An expression with two arithmetic operators
-A function with three parameters.
-At least one logical operator
-Don't forget!:
-
-Test your code! Do the calculations on your own calculator. Check that the computer is producing the same result.
-Remember that functions are sub factories or sub programs of the entire program.
-You are required to make a minimum of 20 commits to your Git repository as you progress and write your code. Failing to do so will result in an automatic 0 for the assignment.*/
-
 //Be a RockStar, Change the world, Become a Computer Programmer
 function highDemand(programmer)/*the function named highDemand with the parameter of Programmer (definition)*/{
 	console.log("Be a RockStar, Change the world, Become a " + programmer);//action the function performs when called
@@ -31,7 +16,7 @@ var programmer = function(genius, determined)/*a variable named programmer equal
 programmer(1, 99);//argues that the programmer variable with the function containing genius and determined has a value of 1 and 99 for its parameters
 var a = programmer(1, 99);//adds a variable to the call function
 var odds = "I like those odds!";//a variable with a string as its definition
-console.log("To become a programmer, you have to be 1% genius and 99% determined. For a total of " + a + "%" + " AWESOME" + " " + odds);
+console.log("To become a programmer, you have to be 1% genius and 99% determined. For a total of " + a + "%" + " AWESOME" + " " + odds);/*finally prints out the result of the functions and variables*/
 
 
 
