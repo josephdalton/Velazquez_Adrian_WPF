3c501473f4db457eda6417ff07cbe1156a3fee57
Adrian Velazquez <adriansane@fullsail.edu>
2013-10-27 10:40:41 -0400

    deleted uneeded commenting

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,14 +2,7 @@
 //October 24th 2013
 //Functions - Industry
 
-/*Make sure each of the sub-projects follows this list of criteria:
-As in the earlier assignments you will need to create code that calculates a result. You may not use the same formulas or code used in previous assignments.
-Decide what your code will calculate. This should be an original calculation. In other words you may not use any of the examples mentioned in this class. This will result in an automatic 0.
-Information should be collected from the user with prompts
-Calculate the result using the variables in an expression (NOT the literal values or numbers)
-The result should appear in the browser's console. (Remember to include units like “square feet” or "meters cubed"). This should explain the result as well.
-Use a function as part of your code in each sub-project. This function should have at least two parameters and return a value.
- 
+/*
 Each of the following requirements must exist in at least one of the 3 subprojects:
 A ternary
 An else if
